05598a907bf13d1de25bc79e2c4a812d88c03951
Flak DiNenno <flak@dinenno.com>
2022-04-04 20:04:14 -0400

    moved Global Constants from userScript to global_constants.js

---
+++ Asana_Tweaks/global_constants.js
@@ -0,0 +1,39 @@
+//==========================
+//  GLOBAL VARs
+//==========================
+
+const tasksSelector = '.SpreadsheetTaskName' +
+  '.SpreadsheetTaskName--editable' +
+  '.SpreadsheetGridTaskNameCell-taskName';
+
+const addElsToThisBarSelector = ".TopbarPageHeaderGlobalActions";
+const completedTaskClassName = "SpreadsheetGridTaskNameCell-taskName--completed";
+const burgerMenu = document.querySelector(".AsanaBaseTopbar--showingBreadcrumbs");
+
+const uiElsToHide = [	//each of these selectors are classes WHICH COULD return MULTIPLE elements
+  ".PageToolbarStructure",
+  ".PageToolbarStructure--withoutBottomBorder",
+  ".PageToolbarStructure.ProjectSpreadsheetGridPageToolbar",
+  ".PageToolbarStructure--withoutBottomBorder.PageToolbarStructure.ProjectSpreadsheetGridPageToolbar",
+
+  ".PageToolbarStructure--withSeparatorDots",
+  ".PageToolbarStructure--withSeparatorDots.PageToolbarStructure--withoutBottomBorder",
+  ".PageToolbarStructure--withSeparatorDots.PageToolbarStructure--withoutBottomBorder.PageToolbarStructure",
+  ".PageToolbarStructure--withSeparatorDots.PageToolbarStructure--withoutBottomBorder.PageToolbarStructure.ProjectSpreadsheetGridPageToolbar",
+
+  ".ProjectSpreadsheetGridPageToolbar",
+  ".ProjectSpreadsheetAddTaskAndAggregationRow",
+  ".ProjectSpreadsheetAddTaskAndAggregationRow-stickyCell.ProjectSpreadsheetAddTaskAndAggregationRow-stickyCell--compact",
+  ".ProjectSpreadsheetAddTaskAndAggregationRow-stickyCell--compact",
+  ".ProjectSpreadsheetAddTaskAndAggregationRow-stickyCell",
+
+  ".SpreadsheetGridHeaderStructure",
+  ".SpreadsheetRow.SpreadsheetRow--enabled.ProjectSpreadsheetAddTaskAndAggregationRow.ProjectSpreadsheetAddTaskAndAggregationRow--compact.ProjectSpreadsheetAddTaskAndAggregationRow-hasSubtaskToggle"
+];
+const uiElsToShrink = [	//each of these selectors are classes WHICH COULD return MULTIPLE elements
+  "div.DropTargetTaskGroupHeader > div > div",
+  "div.PotColumnName"
+];
+
+const completedTaskBgColor = null //"#334433";
+const completedTaskTextColor = null //"#777777";

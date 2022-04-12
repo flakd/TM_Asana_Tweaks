8a7410af3b43cc44c367d8250ea907c8aa5848de
Flak DiNenno <flak@dinenno.com>
2022-04-11 22:28:57 -0400

    added some commented out lines for adding classnames for StyleSheet highlighting instead on inline

--- Asana_Tweaks/bullets.js
+++ Asana_Tweaks/bullets.js
@@ -11,9 +11,9 @@ function getHLCodesAsArray() {
     ["|3", "color", "white"],
 
     ["|4", "backgroundColor", "red"],
-    
+
     ["|5", "backgroundColor", "orange"],
-    
+
     ["|6", "backgroundColor", "purple"],
 
     ["|7", "backgroundColor", "blue"],
@@ -50,48 +50,20 @@ function getHLCodesAsArray() {
   return bullets;
 }
 /*
-  ["|1", "backgroundColor", "white"],
-  ["|1", "color", "black"],
-
-  ["|2", "backgroundColor", "yellow"],
-  ["|2", "color", "black"],
-
-  ["|3", "backgroundColor", "DeepPink"],
-  ["|3", "color", "white"],
-
-  ["|4", "backgroundColor", "red"],
-
-  ["|5", "backgroundColor", "orange"],
-
-  ["|6", "backgroundColor", "purple"],
-
-  ["|7", "backgroundColor", "blue"],
-  ["|7", "color", "white"],
-
-  ["|8", "backgroundColor", "beige"],
-  ["|8", "color", "black"],
-
-  ["|9", "backgroundColor", "white"],
-  ["|9", "color", "black"],
-
-  ["FUP", "backgroundColor", "turquoise"],
-
-  ["<>", "backgroundColor", "brown"],
-  ["<>", "color", "yellow"],
-
-  ["√", "backgroundColor", "purple"],
-  ["√", "color", "white"],
-
-  ["√√", "backgroundColor", "#AAAAAA"],
-  ["√√", "textDecoration", "underline overline"],
-  ["√√", "color", "black"],
-
-  ["~~", "fontStyle", "italic"],
-  ["~~", "textDecoration", "underline overline"],
-  ["~~", "fontWeight", "900"],
-  ["~~", "textShadow", "2px 2px #FF0000"],
-
-  ["[]", "fontVariant", "small-caps"],
-
-  ["{}", "fontWeight", "900"]
+    ["|1", "HStyle01"],
+    ["|2", "HStyle02"],
+    ["|3", "HStyle03"],
+    ["|4", "HStyle04"],
+    ["|5", "HStyle05"],
+    ["|6", "HStyle06"],
+    ["|7", "HStyle07"],
+    ["|8", "HStyle08"],
+    ["|9", "HStyle09"],
+    ["FUP", "HStyle10"],
+    ["<>", "HStyle11"],
+    ["√", "HStyle12"],
+    ["√√", "HStyle13"],
+    ["~~", "HStyle14"],
+    ["[]", "HStyle15"],
+    ["{}", "HStyle16"],
 */

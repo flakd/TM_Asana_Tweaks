17ffb0efb583da628cce5553231895b81cbec9c3
Flak DiNenno <flak@dinenno.com>
2022-04-14 07:46:51 -0400

    trying to change bullet array structure

--- bullets.js
+++ bullets.js
@@ -50,6 +50,19 @@ function getHLCodesAsArray() {
   return bullets;
 }
 /*
+  const bullets = [
+
+    [ 
+      ["|1"], 
+      ["backgroundColor", "white"]
+    ],
+    [ 
+      ["|1"], 
+      ["color", "black"],
+    ]
+  ];
+*/
+/*
     ["|1", "HStyle01"],
     ["|2", "HStyle02"],
     ["|3", "HStyle03"],
@@ -66,4 +79,51 @@ function getHLCodesAsArray() {
     ["~~", "HStyle14"],
     ["[]", "HStyle15"],
     ["{}", "HStyle16"],
+*/
+
+/*
+  ["|1", "backgroundColor", "white"],
+  ["|1", "color", "black"],
+
+  ["|2", "backgroundColor", "yellow"],
+  ["|2", "color", "black"],
+
+  ["|3", "backgroundColor", "DeepPink"],
+  ["|3", "color", "white"],
+
+  ["|4", "backgroundColor", "red"],
+
+  ["|5", "backgroundColor", "orange"],
+
+  ["|6", "backgroundColor", "purple"],
+
+  ["|7", "backgroundColor", "blue"],
+  ["|7", "color", "white"],
+
+  ["|8", "backgroundColor", "beige"],
+  ["|8", "color", "black"],
+
+  ["|9", "backgroundColor", "white"],
+  ["|9", "color", "black"],
+
+  ["FUP", "backgroundColor", "turquoise"],
+
+  ["<>", "backgroundColor", "brown"],
+  ["<>", "color", "yellow"],
+
+  ["√", "backgroundColor", "purple"],
+  ["√", "color", "white"],
+
+  ["√√", "backgroundColor", "#AAAAAA"],
+  ["√√", "textDecoration", "underline overline"],
+  ["√√", "color", "black"],
+
+  ["~~", "fontStyle", "italic"],
+  ["~~", "textDecoration", "underline overline"],
+  ["~~", "fontWeight", "900"],
+  ["~~", "textShadow", "2px 2px #FF0000"],
+
+  ["[]", "fontVariant", "small-caps"],
+
+  ["{}", "fontWeight", "900"]
 */
